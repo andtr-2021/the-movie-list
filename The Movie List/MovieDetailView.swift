7bb71476c52cf1fddccedb95cc959b75eef5bf4b
An Do <s3878698@rmit.edu.vn>
2022-07-21 16:40:29 +0700

    Add RMIT and RMIT Student

---
+++ The Movie List/MovieDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  MovieDetailView.swift
+//  The Movie List
+//
+//  Created by Truong An Do on 21/07/2022.
+//
+
+import SwiftUI
+
+struct MovieDetailView: View {
+    var body: some View {
+        Text("Hello RMIT")
+    }
+}
+
+struct MovieDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        MovieDetailView()
+    }
+}
