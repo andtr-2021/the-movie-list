bd337716f9f0dee647f3d34508d57dedfb4b2a2a
An Do <s3878698@rmit.edu.vn>
2022-07-21 16:23:10 +0700

    Initial Commit

---
+++ The Movie List/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  The Movie List
+//
+//  Created by Truong An Do on 21/07/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ The Movie List/The_Movie_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_Movie_ListApp.swift
+//  The Movie List
+//
+//  Created by Truong An Do on 21/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct The_Movie_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
